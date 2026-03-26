4beeb3329f575aa5ce8b5666ce4ca605f8078908
Maii210 <marielterrazas21@gmail.com>
2026-03-26 11:07:18 -0400

    introduccion a haskell

--- Definicion-de-Funciones.hs
+++ Definicion-de-Funciones.hs
@@ -1,3 +1,8 @@
 --Ejercicio 1
 areaCuadrado :: Int -> Int
 areaCuadrado lado = lado * lado
+
+areaRectangulo :: Int -> Int -> Int
+areaRectangulo base altura = base * altura
+perimetroRectangulo :: Int -> Int -> Int 
+perimetroRectangulo base altura = 2 * (base + altura)
