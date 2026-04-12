0bd4225e17e19291aa190952b4597471c2944f76
Maii210 <marielterrazas21@gmail.com>
2026-04-12 18:37:25 -0400

    Update Principio-de-Currying.hs

--- Principio-de-Currying.hs
+++ Principio-de-Currying.hs
@@ -24,3 +24,30 @@ f8 x y z = x (y z)
 
 f9 :: (tx -> ty -> tz) -> tx -> ty -> tz
 f9 x y z = (x y) z
+
+f10 :: (tx -> ty) -> (tz -> tx) -> (tw -> tz) -> tw -> ty
+f10 x y z w= x (y (z w))
+
+f11 :: (tx -> ty -> tz -> tw) -> tx -> ty -> tz -> tw
+f11 x y z w= ((x y) z) w
+
+f12 x y z w= ((x y) (z w))
+
+f13 :: Int -> Int -> tz -> Int
+f13 x y z = x*2+y
+
+f14 :: (tx -> ty -> tz) -> tx -> ty -> tz
+f14 x y z = x y z
+
+f15 :: Bool -> Bool -> Bool -> Bool
+f15 x y z | x = y
+	        | y = z
+
+f16 :: (tx -> tx -> ty) -> (tz -> tx) -> tz -> ty
+f16 x y z = x (y z)(y z)
+
+c :: (a -> b -> c) -> (a, b) -> c 
+c f (x, y) = f x y
+
+u f x y = f(x, y)
+
